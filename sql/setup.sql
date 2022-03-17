bc6962908bf8c6878bfecf105c9c212bf5689049
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-16 17:50:14 -0700

    basic file setup and added two tables to sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,22 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS bikes;
+DROP TABLE IF EXISTS concerts;
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS guitars;
+DROP TABLE IF EXISTS restaurants;
+
+CREATE TABLE bikes (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
+    manufacturer TEXT NOT NULL,
+    model TEXT NOT NULL,
+    frame_size INT NOT NULL
+);
+
+CREATE TABLE concerts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
+    venue TEXT NOT NULL,
+    band TEXT NOT NULL,
+    date DATE
+);
