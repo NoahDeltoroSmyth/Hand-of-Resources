31eeb452bef285e4bc72394ba73564f1868e198c
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-17 10:55:49 -0700

    baby step complete for getbyid. test and controller

--- __tests__/bikes.test.js
+++ __tests__/bikes.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Bike = require('../lib/models/Bike');
 
 describe('Hand-of-Resources routes', () => {
   beforeEach(() => {
@@ -20,5 +21,15 @@ describe('Hand-of-Resources routes', () => {
     };
     const res = await request(app).post('/api/v1/bikes').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a bike by id', async () => {
+    const bike = await Bike.insert({
+      manufacturer: 'Honda',
+      model: 'CB360',
+      frameSize: 33,
+    });
+    const res = await request(app).get(`/api/v1/bikes/${bike.id}`);
+    expect(res.body).toEqual(bike);
   });
 });
