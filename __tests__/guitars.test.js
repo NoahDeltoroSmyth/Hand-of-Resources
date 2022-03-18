6a8ea234433bc46758c9b64b840ffe04bfec66e1
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-18 16:58:06 -0700

    find guitar by id complete

--- __tests__/guitars.test.js
+++ __tests__/guitars.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Guitar = require('../lib/models/Guitar');
 
 describe('Hand-of-Resources routes', () => {
   beforeEach(() => {
@@ -10,5 +11,25 @@ describe('Hand-of-Resources routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a guitar', async () => {
+    const guitar = {
+      manufacturer: 'Gibson',
+      color: 'Red',
+      stringCount: 6,
+    };
+    const res = await request(app).post('/api/v1/guitars').send(guitar);
+    expect(res.body).toEqual({ id: expect.any(String), ...guitar });
+  });
+
+  it('finds a guitar by id', async () => {
+    const guitar = await Guitar.insert({
+      manufacturer: 'Gibson',
+      color: 'Red',
+      stringCount: 6,
+    });
+    const res = await request(app).get(`/api/v1/guitars/${guitar.id}`);
+    expect(res.body).toEqual(guitar);
   });
 });
