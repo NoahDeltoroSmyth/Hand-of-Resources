7cee8ae50f15da21a489c3b7ea29ac9e2bd61fb7
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-18 15:09:47 -0700

    findbyid complete

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Game = require('../lib/models/Game');
 
 describe('Hand-of-Resources routes', () => {
   beforeEach(() => {
@@ -22,5 +23,13 @@ describe('Hand-of-Resources routes', () => {
     expect(res.body).toEqual({ id: expect.any(String), ...game });
   });
 
-  // it('');
+  it('finds game by id', async () => {
+    const game = await Game.insert({
+      title: 'Crash Bandicoot',
+      genre: 'family fun',
+      price: 2000,
+    });
+    const res = await request(app).get(`/api/v1/games/${game.id}`);
+    expect(res.body).toEqual(game);
+  });
 });
