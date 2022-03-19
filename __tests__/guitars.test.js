f855f62139609670af8c929b871df619ebb154e4
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-18 17:05:17 -0700

    guitar find all complete

--- __tests__/guitars.test.js
+++ __tests__/guitars.test.js
@@ -32,4 +32,20 @@ describe('Hand-of-Resources routes', () => {
     const res = await request(app).get(`/api/v1/guitars/${guitar.id}`);
     expect(res.body).toEqual(guitar);
   });
+
+  it('finds all guitars', async () => {
+    await Guitar.insert({
+      manufacturer: 'les paul',
+      color: 'black',
+      stringCount: 6,
+    });
+    await Guitar.insert({
+      manufacturer: 'martin',
+      color: 'brown',
+      stringCount: 12,
+    });
+    const guitars = await Guitar.findAll();
+    const res = await request(app).get('/api/v1/guitars');
+    expect(res.body).toEqual(guitars);
+  });
 });
