2822e1b388b9ad90308e2a55db408fa53f68eb33
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-20 11:33:04 -0700

    findById for restaurant complete

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Restaurant = require('../lib/models/Restaurant');
 
 describe('Hand-of-Resources routes', () => {
   beforeEach(() => {
@@ -20,6 +21,16 @@ describe('Hand-of-Resources routes', () => {
     };
     const res = await request(app).post('/api/v1/restaurants').send(restaurant);
     expect(res.body).toEqual({ id: expect.any(String), ...restaurant });
+  });
+
+  it('responds with a restaurant by id', async () => {
+    const restaurant = await Restaurant.insert({
+      name: 'Dennys',
+      openAt: '06:00:00',
+      closeAt: '23:00:00',
+    });
+    const res = await request(app).get(`/api/v1/restaurants/${restaurant.id}`);
+    expect(res.body).toEqual(restaurant);
     console.log('res.body', res.body);
   });
 });
